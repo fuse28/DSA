28f86b871ea0261becc90fa36c8eb44e88831e86
fuse28 <mixfuse92@gmail.com>
2022-04-09 22:43:03 +0530

    sorting

---
+++ sorting.js
@@ -0,0 +1,9 @@
+let numbers = document.getElementById("decOrder");
+
+// let arr = [];
+// for (let i = 100; i >= 0; i--) {
+//   let decOrder = 0;
+//   decOrder -= arr.push([i]);
+// }
+// console.log(arr);
+// numbers.innerHTML = arr;
