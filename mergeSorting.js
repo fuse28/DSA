ddfdd52d06ef0b4d43ad445ffaab4047d887ffe0
fuse28 <mixfuse92@gmail.com>
2022-04-12 16:26:09 +0530

    new commit

--- mergeSorting.js
+++ mergeSorting.js
@@ -20,10 +20,35 @@ function merge(arr1, arr2) {
   return arr3;
 }
 
+// function merge(arr1, arr2) {
+//   let arr3 = new Array(arr1.length + arr2.length);
+//   let pointer1 = 0;
+//   let pointer2 = 0;
+
+//   for (let i = 0; i < arr3.length; i++) {
+//     if (pointer1 >= arr1.length) {
+//       arr3[i] = arr2[pointer2];
+//       pointer2++;
+//     } else if (pointer2 >= arr2.length) {
+//       arr3[i] = arr1[pointer1];
+//       pointer1++;
+//     } else if (arr1[pointer1] < arr2[pointer2]) {
+//       arr3[i] = arr1[pointer1];
+//       pointer1++;
+//     } else {
+//       arr3[i] = arr2[pointer2];
+//       pointer2++;
+//     }
+//   }
+
+//   return arr3;
+// }
+
 function mergeSort(arr) {
   if (arr.length <= 1) {
     return arr;
   }
+
   let mid = Math.ceil(arr.length / 2);
   let arr1 = arr.slice(0, mid);
   let arr2 = arr.slice(mid, arr.length);
@@ -33,4 +58,12 @@ function mergeSort(arr) {
 
   return merge(arr1, arr2);
 }
-console.log(mergeSort([2, 4, 1, 6, 7, 9, 8]));
+console.log(mergeSort([25, 63, 12, 56, 47, 39, 48]));
+// let results = mergeSort([25, 63, 12, 56, 47, 39, 48]);
+
+// let MaximumEle = results[0];
+// let MinimumEle = results.splice(-1)[0];
+// let MaxMinEle = (document.getElementById(
+//   "MaxMinEle"
+// ).innerHTML = `Maximum Element is : ${MaximumEle}</br>
+// Minimum Element is : ${MinimumEle}`);
